80002e61031a9196e2cde1e24ad60fa4cb0bfe0f
222112038 <falanarofako29@gmail.com>
2024-08-14 10:14:52 +0700

    update navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { BsDatabaseAdd } from "react-icons/bs";
 import { FiLogOut } from "react-icons/fi";
 import { TbUserSquareRounded } from "react-icons/tb";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const MenuItem = ({ path, label }) => {
   const location = useLocation();
@@ -50,6 +50,10 @@ const ToggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
 
+  const handleLogout = () => {
+    localStorage.clear();
+  };
+
   return (
     <div className="relative lg:hidden">
       <div
@@ -92,6 +96,7 @@ const ToggleMenu = () => {
             <Link
               to="/login"
               className="flex items-center gap-2 px-3 py-2 mx-auto text-base bg-red-500 w-fit rounded-xl"
+              onClick={handleLogout}
             >
               <span>Logout</span>
               <FiLogOut />
@@ -106,19 +111,22 @@ const ToggleMenu = () => {
 const Navbar = () => {
   const [isUserClicked, setIsUserClicked] = useState(false);
   const [userPreferences, setUserPreferences] = useState({});
-
-  const user = {
-    name: "Falana Rofako Hakam",
-    role: "pcl",
-  };
-
+  // const navigate = useNavigate();
+ 
   const handleUserClick = () => {
     setIsUserClicked(!isUserClicked);
   };
 
   useEffect(() => {
-    setUserPreferences(user);
+    setUserPreferences({
+      namaLengkap: localStorage.getItem("namaLengkap"),
+      peran: localStorage.getItem("peran"),
+    });
   }, []);
+
+  const handleLogout = () => {
+    localStorage.clear();
+  };
 
   return (
     <nav className="bg-porange font-asap px-[10%] sticky top-0 z-50">
@@ -137,14 +145,15 @@ const Navbar = () => {
           {isUserClicked && (
             <div className="absolute top-[40px] right-0 w-fit h-fit p-4 rounded-xl bg-base border-[3px] border-porange-30 z-10 text-porange text-center font-asap font-semibold shadow-lg flex flex-col gap-4 items-start">
               <p className="text-[16px] font-semibold whitespace-nowrap leading-none">
-                {userPreferences.name}
+                {userPreferences.namaLengkap}
               </p>
               <p className="text-[14px] font-[500] italic whitespace-nowrap leading-none">
-                {userPreferences.role == "admin" ? "Admin" : "Petugas Pencacah Lapangan (PCL)"}
+                {userPreferences.peran == "admin" ? "Admin" : "Petugas Pencacah Lapangan (PCL)"}
               </p>
               <Link
                 to="/login"
                 className="px-2 py-1 w-fit bg-red-500 text-base text-[14px] font-[500] rounded-xl flex items-center gap-2"
+                onClick={handleLogout}
               >
                 Logout
                 <FiLogOut />
